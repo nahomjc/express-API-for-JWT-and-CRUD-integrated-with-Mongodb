7ff3ad9396f38c89caac7b40459b6f9a78a45e11
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-12 23:47:58 +0300

    feat: add update and get id on routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,19 +1,13 @@
 const express=require('express')
 const router =express.Router()
-const {getContacts,createContact}=require('../controllers/contactController')
+const {getContacts,createContact,updateContact,getContact}=require('../controllers/contactController')
 
 router.route("/").get(getContacts)
 
 router.route("/").post(createContact)
-router.route("/:id").get((req , res)=>{
- 
-    res.status(200).json({message:` Get contact for ${req.params.id}`})
-})
+router.route("/:id").get(getContact)
 
-router.route("/:id").put((req , res)=>{
- 
-    res.status(200).json({message:` Update contact for ${req.params.id}`})
-})
+router.route("/:id").put(updateContact)
 
 router.route("/:id").delete((req , res)=>{
  
