9d05deb31ef6f637056ce47bd7a9b1eae8692da3
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-13 00:28:52 +0300

    chore: arrange the route

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,13 +2,11 @@ const express=require('express')
 const router =express.Router()
 const {getContacts,createContact,updateContact,getContact,deleteContact}=require('../controllers/contactController')
 
-router.route("/").get(getContacts)
+router.route("/").get(getContacts).post(createContact)
 
-router.route("/").post(createContact)
-router.route("/:id").get(getContact)
 
-router.route("/:id").put(updateContact)
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact)
 
-router.route("/:id").delete(deleteContact)
+
 
 module.exports= router
