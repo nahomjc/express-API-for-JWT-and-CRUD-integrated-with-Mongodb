7fc7ad7734caafddf6bb9557b251eaa91d627ce5
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-22 13:08:01 +0300

    chore: mongo db added to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express')
+const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 const dotenv=require("dotenv").config()
 
+connectDb
 const app =express();
 
 
@@ -8,7 +11,7 @@ app.use(express.json())
 
 const port = process.env.PORT|| 5000;
 app.use('/api/contacts', require('./routes/contactRoutes'));
-
+app.use(errorHandler)
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 })
