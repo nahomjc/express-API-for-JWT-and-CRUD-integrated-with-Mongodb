3b58688525656a535df6f7dcd35a30c3b6e39d83
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-22 13:27:49 +0300

    chore: minior update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,7 +4,7 @@ const connectDb = async()=>{
 
     try{
 const connect = await mongoose.connect(process.env.CONNECTION_STRING)
-console.log('Database connected',connect, connect.connection.host,connect.connection.name)
+console.log('Database connected', connect.connection.host,connect.connection.name)
     }
     catch(err){
 console.log(err);
