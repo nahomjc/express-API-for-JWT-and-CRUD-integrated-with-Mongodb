0188d618b27012f50ae35fcf918b23592df048c5
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-27 15:23:55 +0300

    chore:user registration and user validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,30 @@
 const asyncHandler =require("express-async-handler")
+const User = require('../model/userModel')
+const bcrypt=require('bcrypt')
 //@desc Register a user
 //@route Post /api/users/Register
 //@access public
 const registerUser = asyncHandler( async(req,res)=>{
+    const {username , email,password}=req.body;
+    if(!username||!email||!password){
+        res.status(400)
+        throw new Error("All fields are mandatory")
+    }
+    const userAvailable= await User.findOne({email})
+    if(userAvailable){
+        res.status(400)
+        throw new Error("Email is already available")
+    }
+    //Hash Password 
+    const hashedPassword= await bcrypt.hash(password,10)
+    const user =await User.create({username,email,password:hashedPassword})
+    if(user){
+        res.status(201).json({_id:user.id,email:user.email})
+    }else{
+        res.status(404)
+        throw new Error("User data is not Vaild")
+    }
+    console.log("Hash Password",hashedPassword,password)
     res.json({message:"Register the user"})
     })
 //@desc login a user
