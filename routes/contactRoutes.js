bdc5934aeccda87861521af5106b37b344c3ba9e
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-12-05 13:34:19 +0300

    feat: added validateToken to contactRoutes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,10 @@
 const express=require('express')
 const router =express.Router()
 const {getContacts,createContact,updateContact,getContact,deleteContact}=require('../controllers/contactController')
+const validateToken=require("../middleware/validateTokenHandler")
 
+router.use(validateToken)
 router.route("/").get(getContacts).post(createContact)
-
-
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact)
 
 
